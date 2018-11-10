db5dff8435c0cd2b7a20d512d04c83b808593e91
Rafael Santos Brussolo <rafael.hp8@gmail.com>
2018-11-10 13:27:23 -0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HowitworksComponent } from './components/howitworks/howitworks.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { DownloadComponent } from './components/download/download.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    HowitworksComponent,
+    TestimonialsComponent,
+    DownloadComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
